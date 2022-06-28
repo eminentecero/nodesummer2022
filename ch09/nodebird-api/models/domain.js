557715b78938b2037fe0fd355d1a07d69d2dafb8
eminentecero <rapael860429@naver.com>
2022-06-28 15:18:17 +0900

    10단원 완료

---
+++ ch09/nodebird-api/models/domain.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Domain extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            // 인터넷 주소
+            host: {
+                type: Sequelize.STRING(80),
+                allowNull: false,
+            },
+            // 도메인 종류
+            type: {
+                type: Sequelize.ENUM('free', 'premium'),
+                allowNull: false,
+            },
+            // 클라이언트 비밀 키
+            clientSecret: {
+                type: Sequelize.UUID,
+                allowNull: false,
+            },
+        },{
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'Domain',
+            tableName: 'domains',
+        });
+    }
+    static associate(db) {
+        db.Domain.belongsTo(db.User);
+      }
+};
