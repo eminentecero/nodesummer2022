e1747074f95efe63fd54deef78a0a3e56c864d5a
eminentecero <rapael860429@naver.com>
2022-07-05 00:10:06 +0900

    ch09-1  스스로 해보기

--- ch09/nodebird/controller/user.js
+++ ch09/nodebird/controller/user.js
@@ -2,9 +2,11 @@ const User = require('../models/user');
 
 exports.addFollowing = async(req, res, next) => {
     try{
+        console.log('들어오나?');
         const user = await User.findOne({where: {id: req.user.id}});
         if(user){
             await user.addFollowing(parseInt(req.params.id, 10));
+            console.log('잘 되어지나?');
             res.send('success');
         }else{
             res.status(404).send('no user');
@@ -14,3 +16,19 @@ exports.addFollowing = async(req, res, next) => {
         next(error);
     }
 };
+
+exports.unFollowing = async(req, res, next) => {
+    try{
+        const user = await User.findOne({where: {id: req.user.id}});
+        if(user){
+            await user.removeFollowing(parseInt(req.params.id, 10));
+            res.send('success');
+        }else{
+            res.status(404).send('no user');
+        }
+    }catch(error){
+        console.error(error)
+        next(error);
+    }
+}
+
--- ch09/nodebird/routes/user.js
+++ ch09/nodebird/routes/user.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const {isLoggedIn} = require('./middlewares');
 const User = require('../models/user');
-const { addFollowing } = require('../controller/user');
+const { addFollowing, unFollowing } = require('../controller/user');
 
 const router = express.Router();
 
 
-router.post(':/id/follow', isLoggedIn, addFollowing);
+router.post('/:id/follow', isLoggedIn, addFollowing);
+router.post('/:id/unfollow', isLoggedIn, unFollowing);
 
 module.exports = router;
