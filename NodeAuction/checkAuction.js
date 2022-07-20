996afcf13042402b87f76da4a8350a87af22ad6c
eminentecero <rapael860429@naver.com>
2022-07-20 17:12:01 +0900

    ch13-4 스스로 해보기

--- NodeAuction/checkAuction.js
+++ NodeAuction/checkAuction.js
@@ -10,20 +10,27 @@ module.exports = async () => {
                 SoldId: null,
             },
         });
+        
         targets.forEach(async (target) => {
             const end = new Date();
             end.setHours(end.getHours() + target.time);
+            
             if(end < new Date()){
                 const success = await Auction.findOne({
                     where: { GoodId: target.id },
                     order: [['bid', 'DESC']],
                 });
-                await Good.update({ Soldid: success.UserId }, { where: { id: target.id } });
-                await User.update({
-                    money: sequelize.literal(`money - ${success.bid}`),
-                }, {
-                    where: { id: success.UserId },
-                });
+                if(success){
+                    await Good.update({ Soldid: success.UserId }, { where: { id: target.id } });
+                    await User.update({
+                        money: sequelize.literal(`money - ${success.bid}`),
+                    }, {
+                        where: { id: success.UserId },
+                    });
+                }else{
+                    end.setDate(end.getDate()  + 10);
+                }
+
             }else{
                 schedule.scheduleJob(end, async () => {
                     const success = await Auction.findOne({
